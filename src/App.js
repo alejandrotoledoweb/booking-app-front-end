7f6183775dcc2da2843dbed68b90bfa40dfbbb21
Alejandro Toledo <atoledofr@gmail.com>
2021-05-04 12:23:02 -0500

    setup redux store, login actions and navbar component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './containers/Dashboard';
+import Home from './containers/Home';
+
+const App = () => (
+  <section>
+    <>
+      <main>
+        Hello world
+      </main>
+    </>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/user/:user_id/dashboard" component={Dashboard} />
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
+  </section>
+);
+
+export default App;
