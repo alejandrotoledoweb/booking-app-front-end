b6e3f8fa5f66480f885e9a609a6d6f80e65f8419
Alejandro Toledo <atoledofr@gmail.com>
2021-05-04 16:23:05 -0500

    modify the app.js file

--- src/App.js
+++ src/App.js
@@ -3,45 +3,132 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Navbar from './components/Navbar';
-import Dashboard from './containers/Dashboard';
 import Home from './containers/Home';
-import { checkLoginStatus } from './actions/auth';
+import { checkLoginStatus, logout, toggleForm } from './actions/auth';
+import { LOGGED_IN } from './actions/types';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import AppointmentList from './containers/AppointmentList';
+import EngineerList from './containers/RestaurantList';
+import Engineer from './components/Restaurant';
+import styles from './css/app.module.scss';
 
-const App = ({ checkLoginStatus }) => {
+const App = ({
+  checkLoginStatus,
+  status,
+  logoutUser,
+  toggleForm,
+  formFlag,
+}) => {
   useEffect(() => {
     checkLoginStatus();
-  }, [checkLoginStatus]);
+    setTimeout(() => {
+      logoutUser();
+    }, 1200000); // logout the user after 30minutes of inactivity
+  }, [checkLoginStatus, logoutUser]);
+
+  const loggedInScreen = () => (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/user/:user_id/appointments"
+        component={AppointmentList}
+      />
+      <Route exact path="/restaurants" component={EngineerList} />
+      <Route exact path="/restaurants/:id" component={Engineer} />
+    </Switch>
+  );
+
+  const details = () => (
+    <div>
+      <h1 className={`${styles.mainTitle} mb-2`}>Virtual Technical Help</h1>
+      <p
+        className={`${styles.details} font-weight-bolder mx-5 my-4`}
+      >
+        This app is built to connect micronauts (microverse students) who may
+        need technical help to Technical Support Engineers, using microverse
+        (an online training school for remote software developers) as a case
+        study.
+      </p>
+    </div>
+  );
+
+  const authForms = () => {
+    if (formFlag) {
+      return (
+        <article className={styles.authForm}>
+          <Login />
+          <p>
+            New User?
+            {' '}
+            <button
+              type="button"
+              onClick={() => toggleForm()}
+              className="btn btn-primary"
+            >
+              Sign up here
+            </button>
+          </p>
+        </article>
+      );
+    }
+    return (
+      <article className={styles.authForm}>
+        <Signup />
+        <p>
+          Existing User?
+          {' '}
+          <button
+            type="button"
+            onClick={() => toggleForm()}
+            className="btn btn-primary"
+          >
+            Log in here
+          </button>
+        </p>
+      </article>
+    );
+  };
+
   return (
     <BrowserRouter>
-      <div className="App">
+      <main className={`${styles.app}`} id="main">
         <header>
-          <Navbar />
+          <Navbar toggleForm={toggleForm} />
         </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/user/:user_id/dashboard" component={Dashboard} />
-          </Switch>
-        </main>
-      </div>
+        <div
+          className={`${styles.mainContent} mx-5 px-3 px-sm-1 py-5  text-center text-white`}
+        >
+          {status === LOGGED_IN ? loggedInScreen() : (
+            <div>
+              {details()}
+              {authForms()}
+            </div>
+          )}
+        </div>
+      </main>
     </BrowserRouter>
   );
 };
 
-App.defaultProps = {
-  checkLoginStatus: PropTypes.func,
-};
-
 App.propTypes = {
-  checkLoginStatus: PropTypes.func,
+  checkLoginStatus: PropTypes.func.isRequired,
+  status: PropTypes.string.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  toggleForm: PropTypes.func.isRequired,
+  formFlag: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   status: state.auth.loggedIn,
+  formFlag: state.auth.toggleForm,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   checkLoginStatus: () => dispatch(checkLoginStatus()),
+  logoutUser: () => dispatch(logout()),
+  toggleForm: () => dispatch(toggleForm()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
