a15dd89dff9f15dfe7aa062383930a533674d342
Alejandro Toledo <atoledofr@gmail.com>
2021-05-04 10:32:53 -0500

    add first directories to setup the react app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import App from './components/App';
+import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
