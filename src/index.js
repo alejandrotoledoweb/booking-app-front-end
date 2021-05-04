233b624974e99c4b4bc1ac0c5ff311507fb35076
Alejandro Toledo <atoledofr@gmail.com>
2021-05-04 16:24:34 -0500

    modify the general style for fonts and background and elete unneeded files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
-import App from './components/App';
-import store from './app/store';
-import * as serviceWorker from './serviceWorker';
+import store from './store';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <React.StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers/index';
+import rootReducer from './reducers';
 
 const middleware = [thunk];
 
